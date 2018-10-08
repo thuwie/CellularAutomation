38e35e718c6a3df1936745f0feee8c7780c465b7
Kirill <8338050@gmail.com>
2018-10-09 01:40:27 +0300

    cells generation & neighbors counter

--- src/app.ts
+++ src/app.ts
@@ -10,9 +10,10 @@ let Application = PIXI.Application,
 
 
 const cellContainer = new PIXI.Container();
-const cellArray = [];
-const cellSize = 8;
-const borderSize = 128;
+const cellsMatrix = [];
+const cellSize = 30;
+const borderSize = 90;
+const vectors = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]];
 
 let app = new Application({
         width: borderSize,
@@ -27,37 +28,71 @@ let app = new Application({
 document.getElementById("display").appendChild(app.view);
 
 
-for (let i = 0; i < 4; i++) {
-    cellArray[i] = [];
-    for (let j = 0; j < 4; j++) {
-        cellArray[i][j] = (Math.random() >= 0.5);
+for (let i = 0; i < borderSize / cellSize; i++) {
+    cellsMatrix[i] = [];
+    for (let j = 0; j < borderSize / cellSize; j++) {
+        cellsMatrix[i][j] = { status: (Math.random() >= 0.5) };
     }
 }
 
-cellArray.forEach((cellSubArray, row) => {
+formCellsSequense(cellsMatrix);
+drawObjects();
+
+cellsMatrix.forEach((cellSubArray, row) => {
     cellSubArray.forEach((cellValue, column) => {
-        //console.log(createCell(cellValue, row, column));
-        console.log(row, column);
-        app.stage.addChild(drawCell(createCell(cellValue, row, column)));
+        console.log(`Pos ${row},${column}. C: `, checkCell(cellsMatrix, row, column));
     });
 });
 
-console.log(cellArray);
-
-function createCell(cell, row, column) {
-    return {
-        status: cell,
-        x: column * cellSize,
-        y: row * cellSize
-    };
+function checkCell(array, x, y) {
+    let count = 0;
+    vectors.forEach(vector => {
+        const checkPosX = x + vector[0];
+        const checkPosY = y + vector[1];
+        if ((checkPosX >= 0 && checkPosY >= 0)
+            && (checkPosX < borderSize / cellSize && checkPosY < borderSize / cellSize)
+            && array[checkPosX][checkPosY].status
+        ) {
+            count++;
+        }
+    });
+    return count;
 }
 
-function drawCell(cell) {
-    const rect = new Graphics();
+function formCell(cell, row, column) {
+    cell.x = column * cellSize;
+    cell.y = row * cellSize;
+    return cell;
+}
+
+function createCell(cell) {
+    let rect = new Graphics();
+    rect.beginFill(cell.status ? 0x000000 : 0xffffff);
     rect.drawRect(cell.x, cell.y, cellSize, cellSize);
-    rect.beginFill(cell.status ? 0x000000 : 0xFFFFFF);
-    return rect;
+    rect.endFill();
+    cell.graphics = rect;
+    return cell;
+}
+
+function formCellsSequense(cellsMatrix) {
+    let cellsSequence = [];
+    cellsMatrix.forEach((cellSubArray, row) => {
+        cellSubArray.forEach((cellObject, column) => {
+            formCell(cellObject, row, column);
+            createCell(cellObject);
+
+        });
+    });
+    return cellsSequence;
+}
+
+function drawObjects() {
+    cellsMatrix.forEach((cellSubArray) => {
+        cellSubArray.forEach((cellObject) => {
+            app.stage.addChild(cellObject.graphics);
+        });
+    });
 }
 
 
-// app.stage.addChild(Grid);
+
