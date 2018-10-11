9fb0076256c57b363fc83fbd4875fe1bc7d1f8d1
Kirill <8338050@gmail.com>
2018-10-12 00:11:39 +0300

    shitofuck

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # CellularAutomation
-[Try it](https://thuwie.github.io/CellularAutomation/)  
-js + webgl is not very polite so its killing my laptop on  64x64 grid hehe
+[Try it](https://thuwie.github.io/CellularAutomation/)
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,45 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 const path = require('path');
 
+const distDir = path.resolve(__dirname, 'dist');
+
 module.exports = {
-    entry: './src/scripts/app.js',
+    entry: './src/scripts/app.ts',
+
+    // What files webpack will manage
+    resolve: {
+        extensions: ['.js', '.ts', '.tsx']
+    },
+
+    // Make errors mor clear
+    devtool: 'inline-source-map',
+
+    // Configure output folder and file
     output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    }
+        path: distDir,
+        filename: 'main_bundle.js'
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: 'ts-loader'
+            }
+        ]
+    },
+
+    devServer: {
+        contentBase: './dist'
+    },
+
+
+    plugins: [
+        new CleanWebpackPlugin([distDir]),
+        new HtmlWebpackPlugin({
+            template: 'src/html/index.html'
+        })
+    ]
 };
